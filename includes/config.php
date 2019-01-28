e76d0d4011ad02ee64d33d21583d845bbc3b7f58
ian andrew macalisang <ian@genwarp.com>
2019-01-28 13:31:13 +0400

    Updated credential for heroku

--- includes/config.php
+++ includes/config.php
@@ -1,8 +1,8 @@
 <?php
-define('DB_SERVER', '');
-define('DB_USERNAME', '');
-define('DB_PASSWORD', '');
-define('DB_NAME', '');
+define('DB_SERVER', '192.185.19.9');
+define('DB_USERNAME', 'andyware_heroku');
+define('DB_PASSWORD', 'ilm10998');
+define('DB_NAME', 'andyware_rsvp');
  
 $con = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
  
