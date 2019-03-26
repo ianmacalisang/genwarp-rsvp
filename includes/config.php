6133b7b650da4bcb1338b09abc3f28c87a90d05f
Ian Andrew Macalisang <39157186+ianmacalisang@users.noreply.github.com>
2019-03-26 15:10:55 +0400

    update heroku remote db

--- includes/config.php
+++ includes/config.php
@@ -1,11 +1,15 @@
 <?php
-define('DB_SERVER', '192.185.19.9');
-define('DB_USERNAME', 'andyware_heroku');
-define('DB_PASSWORD', 'ilm10998');
-define('DB_NAME', 'andyware_rsvp');
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
  
 $con = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+
+$server = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$db = substr($url["path"], 1);
  
+$con = new mysqli($server, $username, $password, $db);
+
 if($con === false){
     die("ERROR: Could not connect. " . mysqli_connect_error());
 }
